73c8cb6cf1ae7840cfbd74038c2035f2afa63e2a
thechaoskrys <krystalbsreid@gmail.com>
2023-11-28 17:42:43 -0500

    complete ops2 challenge.

--- code-challenges/ops2-append.sh
+++ code-challenges/ops2-append.sh
@@ -5,7 +5,7 @@
 # Date of latest revision:      11/28/2023
 # Purpose:                      copy /var/log/syslog to current working dir. appends the current date & time to filename.
 # Execution                     bash ops2-append.sh or ./ops2-append.sh or chmod +x ops2-append.sh
-# Citations:                    roger's class demo, class repo and chatgpt for understanding
+# Citations:                    roger's class demo, class repo and chatgpt for understanding https://chat.openai.com/share/86471729-3376-4877-8050-64747257f990
 
 # copy syslog to the current directory
 cp /var/log/syslog .
