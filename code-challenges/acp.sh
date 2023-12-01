57b6efa4202103197130fc5718106ba458468f44
thechaoskrys <krystalbsreid@gmail.com>
2023-12-01 16:34:50 -0500

    push

--- code-challenges/acp.sh
+++ code-challenges/acp.sh
@@ -4,7 +4,7 @@ pushtogit(){
     git add .
     read -p "commit message: " message
     git commit -m "$message"
-    git push
+    git push origin main
 # i want to insert line break here
     git status
 }
--- code-challenges/ops05-logs.sh
+++ code-challenges/ops05-logs.sh
@@ -5,9 +5,42 @@
 # Date of latest revision:      12/01/2023
 # Purpose:                      clearing logs
 # Execution                     bash ops05-logs.sh or ./ops05-logs.sh or chmod +x ops05-logs.sh
-# Citations:                    ops301 repo https://github.com/codefellows/seattle-ops-301d14/blob/main/class-05/challenges/ASSIGNMENT.md
+# Citations:                    ops301 repo https://github.com/codefellows/seattle-ops-301d14/blob/main/class-05/challenges/ASSIGNMENT.md, [ChatGPT](https://chat.openai.com/share/a039d9e6-30af-42ec-bf99-4a1f0064edbe)
 
-# Identify variables: log files
-log_files=("/var/log/syslog" "/var/log/wtmp")
+# Identify variables: log files, backup directory, and timestamp
+logfiles=("/var/log/syslog" "/var/log/wtmp")
+backupdir="/var/log/backups/"
+timestamp=$(date "+%Y%m%d%H%M%S")
 
 # Print to the screen the file size of the log files before compression
+print_file_size(){
+    file=$1                                     # $1 is a special argument that represents an argument
+    size=$(du -h "$file" | awk '(print $1')     # du gets the disk usage of file and extracts file size. awk commands print $1 to print the first field of each line
+    echo "File size of $file: $size"
+}
+
+# Creating a backup directory if it doesn't already exist
+mkdir -p "backupdir"                            # -p ensures there is no error in the script if the directory already exists. 
+
+# Compress the contents of the log files listed below to a backup directory - the file name should contain a time stamp
+compress_log_file() {
+    file=$1
+    compressed_file="$backupdir/$(basename "$file")-$timestamp.zip"
+    gzip -c "$file" > "$compressedfiles"
+    echo "Compressed $file to $compressedfiles"
+    print_file_size "compressedfiles"
+}
+
+# Clear the contents of the log file
+clear_log_file() {
+    file=$1
+    echo "" > "$file"                              # clears the content of the file by overwriting it with an empty string.
+    echo "Contents of $file has been cleared."
+}
+
+# Print to the screen the file size of the compressed file
+
+# Compare the size of the compressed files to the orginal log files.
+ogsize=$(du -h "$file" | awk '{print $1}')
+compressedsize=$(du -h "$compressedfiles" | awk '{print $1}')
+echo Compare the OG size: $ogsize to the compressed size: $compressedsize"
