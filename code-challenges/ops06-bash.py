fe51fd9d40228016f79ed89d7ba79660c163bd19
thechaoskrys <krystalbsreid@gmail.com>
2023-12-16 16:15:57 -0500

    challenge 6

---
+++ code-challenges/ops06-bash.py
@@ -0,0 +1,28 @@
+# Script Name:                          ops06-bash.py
+# Author name:                          Krystal Reid
+# Date of latest revision:              12/16/2023
+# Purpose:                              bash prompts in python
+# Execution:                            python3 ops06-bash.py
+# Additional Resources                  https://chat.openai.com/share/6ba3377b-9b6f-4d84-9765-3fc3387fa5d9
+
+# Import the os module
+import os
+
+# Declare three variables
+user_name = os.getenv('USER')
+ip_address = os.popen('ip a | grep inet | grep -v 127.0.0.1 | awk \'{print $2}\' | cut -d"/" -f1').read().strip()
+hardware_info = os.popen('lshw -short').read()
+
+# Print variable values
+print(f"Username: {user_name}")
+print(f"IP Address: {ip_address}")
+
+# Execute and print the result of bash commands
+print("\nExecuting 'whoami' command:")
+os.system("whoami")
+
+print("\nExecuting 'ip a' command:")
+os.system("ip a")
+
+print("\nExecuting 'lshw -short' command:")
+print(hardware_info)
